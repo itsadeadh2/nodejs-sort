68caa73e543222e63af72277546d4f2cd6d64744
Thiago <thiago.dasilva@inbrace.com>
2024-06-21 12:53:16 -0400

    Update code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello from Express on AWS Lambda!' });
 });
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
 
 module.exports.handler = serverless(app);
