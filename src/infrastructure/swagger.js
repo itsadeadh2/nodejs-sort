15db6a40a8f2e4ee6b5a20ab674bcf77c4559615
itsadead <itsadeadh2@gmail.com>
2024-06-21 16:01:03 -0400

    Remove unnecessary logic

--- src/infrastructure/swagger.js
+++ src/infrastructure/swagger.js
@@ -1,7 +1,6 @@
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerDocument = require('../swagger.json');
 
-swaggerDocument.servers[0].url = process.env.LAMBDA_URL || 'http://localhost:3000';
 const options = {
     definition: swaggerDocument,
     apis: [`${__dirname}/router.js`], // Path to the API docs
